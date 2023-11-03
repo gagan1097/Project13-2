0551ffe0ed8a3e3c01a87d85e34ba53750b5c4ba
Gagan <gagansodhi751@gmail.com>
2023-11-03 15:06:52 -0600

    Project13-2

---
+++ script.js
@@ -0,0 +1,10 @@
+// This function is from hackerrank.com. This function adds the sum of two integers.
+function sum(a, b)
+{
+  return a + b;
+}
+let num1 = 7;
+let num2 = 10;
+let addition = sum(num1, num2);
+console.log(addition);
+
